34df28150e13f294f521fbccb96f996df4673d8b
vehseri <ireshev@mail.ru>
2017-01-07 21:56:24 +0300

    added text and buttons

--- app/src/main/java/com/vehseri/rpttr/MainActivity.java
+++ app/src/main/java/com/vehseri/rpttr/MainActivity.java
@@ -2,12 +2,40 @@ package com.vehseri.rpttr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textLoginAs;
+    Button buttonAsTeacher, buttonAsPupil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        setButtonBehavior();
+    }
+    private void initViews() {
+        textLoginAs = (TextView)findViewById(R.id.textLoginAs);
+        buttonAsPupil = (Button)findViewById(R.id.buttonAsPupil);
+        buttonAsTeacher = (Button)findViewById(R.id.buttonAsTeacher);
+    }
+    private void setButtonBehavior(){
+        buttonAsPupil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        buttonAsTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
