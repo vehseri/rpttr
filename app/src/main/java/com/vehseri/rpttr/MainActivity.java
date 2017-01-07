3b11f3cad0117810e410df1d07b054972829de19
vehseri <ireshev@mail.ru>
2017-01-07 22:33:30 +0300

    добавил слушателей на кнопки с выводом Toast

--- app/src/main/java/com/vehseri/rpttr/MainActivity.java
+++ app/src/main/java/com/vehseri/rpttr/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,13 +28,13 @@ public class MainActivity extends AppCompatActivity {
         buttonAsPupil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(MainActivity.this, R.string.pupil, Toast.LENGTH_SHORT).show();
             }
         });
         buttonAsTeacher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(MainActivity.this, R.string.teacher, Toast.LENGTH_SHORT).show();
             }
         });
 
